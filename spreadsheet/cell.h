71b8ddf67ad585fddc8d09d7ae3f81d6a4c98935
Ruslan5910 <150811455+Ruslan5910@users.noreply.github.com>
2025-03-08 11:23:39 +0300

    Update cell.h

--- spreadsheet/cell.h
+++ spreadsheet/cell.h
@@ -3,34 +3,75 @@
 #include "common.h"
 #include "formula.h"
 
-#include <functional>
 #include <unordered_set>
+#include <optional>
 
 class Sheet;
+
+class Impl {
+public:
+    virtual ~Impl() = default;
+    
+    virtual std::string GetText() = 0;
+    virtual CellInterface::Value GetValue() = 0;
+    virtual std::vector<Position> GetDependentCells() const;
+    virtual bool HasValue() const;
+    virtual void InvalidateCache() const {}
+};
+
+class EmptyImpl : public Impl {
+public:
+    std::string GetText() override;
+    CellInterface::Value GetValue();
+};
+
+class TextImpl : public Impl {
+public:
+    TextImpl(std::string text)
+        : text_(text) {}
+
+    std::string GetText() override;
+    CellInterface::Value GetValue();
+private:
+    std::string text_;
+};
+
+class FormulaImpl : public Impl {
+public:
+    FormulaImpl(std::string text, SheetInterface& sheet_);
+
+    std::string GetText();
+    CellInterface::Value GetValue();
+
+    std::vector<Position> GetDependentCells() const;
+
+    void InvalidateCache() const;
+
+    bool HasValue() const;
+    
+private:
+    SheetInterface& sheet_;
+    std::unique_ptr<FormulaInterface> formula_;
+    mutable std::optional<FormulaInterface::Value> cache_;
+};
 
 class Cell : public CellInterface {
 public:
     Cell(Sheet& sheet);
-    ~Cell();
-
+    ~Cell() = default;
     void Set(std::string text);
     void Clear();
-
     Value GetValue() const override;
     std::string GetText() const override;
     std::vector<Position> GetReferencedCells() const override;
-
     bool IsReferenced() const;
+    std::unordered_set<Cell*>& GetDependentCells();
 
 private:
-    class Impl;
-    class EmptyImpl;
-    class TextImpl;
-    class FormulaImpl;
-
     std::unique_ptr<Impl> impl_;
-
-    // Добавьте поля и методы для связи с таблицей, проверки циклических 
-    // зависимостей, графа зависимостей и т. д.
-
+    Sheet& sheet_reference_;
+    std::unordered_set<Cell*> dependent_cells_; // ячейки которые зависят
+    std::unordered_set<Cell*> which_depends_on_; // от которых зависит
+    bool FindCircularDependences(const Impl& impl_copy);
+    void InvalidateDependentCellsCache() const;
 };
