10581cae5a701301af6e3344975677148c520cd7
Ruslan5910 <150811455+Ruslan5910@users.noreply.github.com>
2025-03-08 11:20:27 +0300

    Update FormulaAST.h

--- spreadsheet/FormulaAST.h
+++ spreadsheet/FormulaAST.h
@@ -23,25 +23,17 @@ public:
     FormulaAST& operator=(FormulaAST&&) = default;
     ~FormulaAST();
 
-    double Execute(/*добавьте нужные аргументы*/ args) const;
+    double Execute(std::function<double(Position)> args) const;
     void PrintCells(std::ostream& out) const;
     void Print(std::ostream& out) const;
     void PrintFormula(std::ostream& out) const;
 
-    std::forward_list<Position>& GetCells() {
-        return cells_;
-    }
+    std::forward_list<Position>& GetCells();
 
-    const std::forward_list<Position>& GetCells() const {
-        return cells_;
-    }
+    const std::forward_list<Position>& GetCells() const;
 
 private:
     std::unique_ptr<ASTImpl::Expr> root_expr_;
-
-    // physically stores cells so that they can be
-    // efficiently traversed without going through
-    // the whole AST
     std::forward_list<Position> cells_;
 };
 
